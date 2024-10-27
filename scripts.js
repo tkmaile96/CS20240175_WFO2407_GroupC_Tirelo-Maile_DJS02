06cd22bbc8ed46fbc86feaf20210d19aa460d041
Teekay <tkmaile96@gmail.com>
2024-10-27 13:48:40 +0200

    added input validation and error handling

--- scripts.js
+++ scripts.js
@@ -4,10 +4,21 @@ const result = document.querySelector("[data-result]");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
 
-  let{dividend, divider} = object.fromEntries(entries);
+  let{dividend, divider} = Object.fromEntries(entries);
   dividend = parseFloat(dividend);
   divider = parseFloat(divider);
+
+  //check is unputs are provided
+  if  (isNaN(dividend) || isNaN(divider)) {
+    if(dividend === ""  || divider === "") {
+      result.innerText = "Both values are required to perfome  the division.";
+      return
+    } else {
+      result.innerText = "Math Error";
+    }
+  }
+
+
   result.innerText = dividend / divider;
 });
